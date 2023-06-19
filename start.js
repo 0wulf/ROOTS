9530bdad0434766982e07b75dbe1dc0c6c319589
0wulf <francisco.wulf@uc.cl>
2023-06-19 18:17:31 -0400

    final deploy ojala

--- start.js
+++ start.js
@@ -6,13 +6,21 @@ async function deploy() {
   console.log("Installing dependencies...");
   console.log(output1);
 
-  const { stdout: output2 } = await exec("yarn db:migrate");
-  console.log("Migrating database...");
+  const { stdout: output2 } = await exec("yarn db:migrate:undo:all");
+  console.log("Undo migrating database...");
   console.log(output2);
 
-  const { stdout: output3 } = await exec("yarn db:seed");
-  console.log("Seeding database...");
+  const { stdout: output3 } = await exec("yarn db:migrate");
+  console.log("Migrating database...");
   console.log(output3);
+
+  const { stdout: output4 } = await exec("yarn db:seed:undo:all");
+  console.log("Unseeding database...");
+  console.log(output4);
+
+  const { stdout: output5 } = await exec("yarn db:seed");
+  console.log("Seeding database...");
+  console.log(output5);
 }
 
 deploy();
